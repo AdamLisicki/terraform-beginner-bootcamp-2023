df0ee55afdabe9551bcfddb3c822db15cfafadf6
Adam Lisicki <adam.lisicki@adamlisicki.pl>
2023-09-24 22:42:28 +0000

    fix typos in resource-storage.tf

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -38,8 +38,8 @@ resource "aws_s3_object" "upload_assets" {
   for_each = fileset(var.assets_path, "*.{jpg,jpeg,png,gif}")
   bucket = aws_s3_bucket.website_bucket.bucket
   key = "assets/${each.key}"
-  source = "${var_assets_path}/${each.key}"
-  etag = filemd5("${var_assets_path}/${each.key}")
+  source = "${var.assets_path}/${each.key}"
+  etag = filemd5("${var.assets_path}/${each.key}")
   lifecycle {
     replace_triggered_by = [terraform_data.content_version.output]
     ignore_changes = [etag]
