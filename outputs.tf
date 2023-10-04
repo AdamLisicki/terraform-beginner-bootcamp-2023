cebea758bc76383b8c5d63acc3362e7efeca4d3c
Adam Lisicki <96197101+AdamLisicki@users.noreply.github.com>
2023-10-04 20:42:50 +0200

    #56 add outputs for multi home (#57)

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-# output "bucket_name" {
-#   description = "Bucket name for our static website hosting"
-#   value = module.home_tft_hosting.bucket_name
-# }
+output "bucket_name" {
+  description = "Bucket name for our static website hosting"
+  value = {for k in keys(local.hostings) : k => module.terrahome_aws[k].bucket_name}
+}
 
-# output "s3_website_endpoint" {
-#   description = "S3 Static Website hosting endpoint"
-#   value = module.home_tft_hosting.website_endpoint
-# }
+output "s3_website_endpoint" {
+  description = "S3 Static Website hosting endpoint"
+  value = {for k in keys(local.hostings) : k => module.terrahome_aws[k].website_endpoint}
+}
 
-# output "cloudfront_url" {
-#   description = "The CloudFront Distribution Domain Name"
-#   value = module.home_tft_hosting.domain_name
-# }
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = {for k in keys(local.hostings) : k => module.terrahome_aws[k].domain_name}
+}
