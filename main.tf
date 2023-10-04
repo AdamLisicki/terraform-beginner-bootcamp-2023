efb1befbe15e59cde56bb69f7cd36059e380f366
Adam Lisicki <adam.lisicki@adamlisicki.pl>
2023-10-04 08:17:01 +0000

    #48 test our terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -15,30 +15,29 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  # bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!!!!!!"
+  name = "How to play Teamfight Tactics"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Moders have removed all the orginals making this game really fun
-to play (despite that old look graphics). This is my guide that will 
-show you how to play arcanum without spoiling the plot.
+Teamfight Tactics (TFT) is an auto battler game developed and published by Riot Games. 
+The game is a spinoff of League of Legends and is based on Dota Auto Chess, 
+where players compete online against seven other opponents by building a team 
+to be the last one standing.
   DESCRIPTION
-  # domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "342fsdgsdg.cloudfront.net"
-  town =  "gamers-grotto"
-  content_version = 1
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town =  "missingo"
+  content_version = var.content_version
   
 }
